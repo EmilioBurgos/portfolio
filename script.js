25073e374558f15b9850b1280f929eb435bad57e
Emilio Burgos <miliomax@hotmail.com>
2021-03-17 16:03:50 -0300

    menu modal

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+function abrirModal() {
+    document.getElementById("modal").style.top = "0";
+}
+function FecharModal() {
+    document.getElementById("modal").style.top = "-100%";
+}
