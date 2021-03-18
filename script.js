566420bcdbeec9dd74d358ee3030f045c0a33cde
Emilio Burgos <miliomax@hotmail.com>
2021-03-18 18:29:47 -0300

    slider images

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function abrirModal() {
 function FecharModal() {
     document.getElementById("modal").style.top = "-100%";
 }
+
+window.load = slide(1);
+
+var bgNumber = 1;
+
+function slide(n) {
+    var allBgs = 9;
+    document.getElementById("modelos").style.backgroundImage = "url('imagens/"+n+".jpg')";
+}
+
+function anterior() {
+    if(bgNumber>1){
+        bgNumber--;
+        slide(bgNumber);
+    }
+}
+function proximo() {
+    if(bgNumber<9){
+        bgNumber++;
+        slide(bgNumber);
+    }
+}
